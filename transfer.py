409185064c21953bc394c87d99fe7d8a1294a432
ultrayusuf29 <ultrayusuf29@gmail.com>
2018-09-06 14:10:28 +0300

    first_save

---
+++ transfer.py
@@ -0,0 +1,21 @@
+import serial
+import time
+
+#seri port ayarlandi
+ser = serial.Serial("/dev/serial0", baudrate=9600, timeout=1.0)
+
+if (ser.isOpen() == False):
+	ser.Open()
+	print('port actim')
+
+	
+SMS='Selamin Aleykum Ben Omer'
+
+ser.flushInput()
+
+for i,c in enumerate(SMS):
+	print("gonder:" + SMS[0:i])
+
+ser.write((SMS[0:i]+"\n"))
+time.sleep(0.1)
+ser.close()
