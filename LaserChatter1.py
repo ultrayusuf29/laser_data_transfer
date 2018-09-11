1f108a53dc3e5f61d9446cfc0b23f85a5c0bfb91
ultrayusuf29 <ultrayusuf29@gmail.com>
2018-09-11 14:11:58 +0300

    Add files via upload

--- LaserChatter1.py
+++ LaserChatter1.py
@@ -1,7 +1,7 @@
 import serial
 from threading import Thread
 
-ser=serial.Serial("/dev/pts/3",baudrate=9600,timeout=1.0)
+ser=serial.Serial("/dev/pts/4",baudrate=9600,timeout=1.0)
 if (ser.isOpen()==False):
     ser.Open()
     print("Port acildi.")
@@ -11,13 +11,13 @@ ser.flushInput()
 def send_message(x):
         if x!='':
             ser.write(x+"\n")
-            print(x,'\n (Gonderildi) \n')
+            print(x,' (Gonderildi)')
             
 def receive_message():
     while 1:
         data=ser.readline()
         if data:
-            print("alindi:"+data)
+            print("\n alindi:\n"+data)
 if __name__=='__main__':
     r=Thread(target=receive_message)
     r.start()
--- threaded1.py
+++ threaded1.py
@@ -2,12 +2,12 @@ import serial
 import time
 from threading import Thread
 
-ser = serial.Serial("/dev/pts/3", baudrate=9600, timeout=1.0)
+ser = serial.Serial("/dev/pts/4", baudrate=9600, timeout=1.0)
 if (ser.isOpen() == False):
 	ser.Open()
         print('port actim')
 ser.flushInput()
-SMS="yusuf"
+SMS="omer"
 def receive():
     while True:
 	data = ser.readline()
@@ -15,12 +15,12 @@ def receive():
 		print ("\n alindi:\n"+data[:-1])
         time.sleep(0.1)
 def transfer():
-    while 1:
+        while 1:
             for i,c in enumerate(SMS):
                 print("gonder:" + SMS[0:i])
                 ser.write((SMS[0:i]+"\n"))
                 time.sleep(0.1)
-    
+
 
 if __name__=='__main__':
     birinci=Thread(target=receive)
